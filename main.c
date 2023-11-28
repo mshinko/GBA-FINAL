92b326326dabee1ba26ed3fccc4f7180cfbb9ee9
Matthew <mshinko@mail.umw.edu>
2023-11-28 14:21:12 -0500

    Main with new bgs

--- main.c
+++ main.c
@@ -8,17 +8,17 @@
 
 /* include the background image we are using */
 #include "background.h"
-
+#include "baseplate.h"
 /* include the sprite image we are using */
 #include "square.h"
 #include "obstacles.h"
 /* include the tile map we are using */
-#include "map.h"
 #include "obby1.h"
 /* the tile mode flags needed for display control register */
 #define MODE0 0x00
 #define BG0_ENABLE 0x100
-
+#define BG1_ENABLE 0x200
+#define BG2_ENABLE 0x400
 /* flags to set sprite handling in display control register */
 #define SPRITE_MAP_2D 0x0
 #define SPRITE_MAP_1D 0x40
@@ -27,6 +27,11 @@
 
 /* the control registers for the four tile layers */
 volatile unsigned short* bg0_control = (volatile unsigned short*) 0x4000008;
+volatile unsigned short* bg1_control = (volatile unsigned short*) 0x400000a;
+volatile unsigned short* bg2_control = (volatile unsigned short*) 0x400000c;
+volatile unsigned short* bg3_control = (volatile unsigned short*) 0x400000e;
+
+
 
 /* palette is always 256 colors */
 #define PALETTE_SIZE 256
@@ -55,6 +60,12 @@ volatile unsigned short* buttons = (volatile unsigned short*) 0x04000130;
 /* scrolling registers for backgrounds */
 volatile short* bg0_x_scroll = (unsigned short*) 0x4000010;
 volatile short* bg0_y_scroll = (unsigned short*) 0x4000012;
+volatile short* bg1_x_scroll = (unsigned short*) 0x4000014;
+volatile short* bg1_y_scroll = (unsigned short*) 0x4000016;
+volatile short* bg2_x_scroll = (unsigned short*) 0x4000018;
+volatile short* bg2_y_scroll = (unsigned short*) 0x400001a;
+
+
 
 /* the bit positions indicate each button - the first bit is for A, second for
  * B, and so on, each constant below can be ANDED into the register to get the
@@ -127,7 +138,18 @@ void memcpy16_dma(unsigned short* dest, unsigned short* source, int amount) {
     *dma_destination = (unsigned int) dest;
     *dma_count = amount | DMA_16 | DMA_ENABLE;
 }
-
+/*
+void memcpy17_dma(unsigned short* dest, unsigned short* source, int amount) {
+    *dma_source = (unsigned int) source;
+    *dma_destination = (unsigned int) dest;
+    *dma_count = amount | DMA_17 | DMA_ENABLE;
+}
+void memcpy18_dma(unsigned short* dest, unsigned short* source, int amount) {
+    *dma_source = (unsigned int) source;
+    *dma_destination = (unsigned int) dest;
+    *dma_count = amount | DMA_18 | DMA_ENABLE;
+}
+*/
 /* function to setup background 0 for this program */
 void setup_background() {
 
@@ -139,7 +161,7 @@ void setup_background() {
             (obstacles_width * obstacles_height) / 2);
 
     /* set all control the bits in this register */
-    *bg0_control = 0 |    /* priority, 0 is highest, 3 is lowest */
+    *bg0_control = 3 |    /* priority, 0 is highest, 3 is lowest */
         (0 << 2)  |       /* the char block the image data is stored in */
         (0 << 6)  |       /* the mosaic flag */
         (1 << 7)  |       /* color mode, 0 is 16 colors, 1 is 256 colors */
@@ -148,7 +170,32 @@ void setup_background() {
         (0 << 14);        /* bg size, 0 is 256x256 */
 
     /* load the tile data into screen block 16 */
-    memcpy16_dma((unsigned short*) screen_block(16), (unsigned short*) map, map_width * map_height);
+    memcpy16_dma((unsigned short*) screen_block(16), (unsigned short*) background_data, background_width * background_height);
+    /* set all control the bits in this register */
+    *bg1_control = 2 |    /* priority, 0 is highest, 3 is lowest */
+        (0 << 2)  |       /* the char block the image data is stored in */
+        (0 << 6)  |       /* the mosaic flag */
+        (1 << 7)  |       /* color mode, 0 is 16 colors, 1 is 256 colors */
+        (16 << 8) |       /* the screen block the tile data is stored in */
+        (1 << 13) |       /* wrapping flag */
+        (0 << 14);        /* bg size, 0 is 256x256 */
+
+    /* load the tile data into screen block 16 */
+    memcpy16_dma((unsigned short*) screen_block(17), (unsigned short*) baseplate_data, baseplate_width * baseplate_height);
+    /* set all control the bits in this register */
+    *bg2_control = 1 |    /* priority, 0 is highest, 3 is lowest */
+        (0 << 2)  |       /* the char block the image data is stored in */
+        (0 << 6)  |       /* the mosaic flag */
+        (1 << 7)  |       /* color mode, 0 is 16 colors, 1 is 256 colors */
+        (16 << 8) |       /* the screen block the tile data is stored in */
+        (1 << 13) |       /* wrapping flag */
+        (0 << 14);        /* bg size, 0 is 256x256 */
+
+    /* load the tile data into screen block 16 */
+    memcpy16_dma((unsigned short*) screen_block(18), (unsigned short*) obby1_data, obby1_width * obby1_height);
+
+
+
 }
 
 /* just kill time */
@@ -474,9 +521,9 @@ void square_update(struct Square* square, int xscroll,int yscroll) {
     }
 
     /* check which tile the koopa's feet are over */
-    unsigned short tile = tile_lookup(square->x + 8, square->y+16, xscroll, 0, map, map_width, map_height);
+    unsigned short tile = tile_lookup(square->x + 8, square->y+16, xscroll, 0, baseplate_data, baseplate_width, baseplate_height);
     
-    unsigned short TC= tile_lookup(square->x+8, square->y, xscroll, 0, map, map_width, map_height);
+    unsigned short TC= tile_lookup(square->x+8, square->y, xscroll, 0, baseplate_data, baseplate_width, baseplate_height);
     /* if it's block tile
      * these numbers refer to the tile indices of the blocks the koopa can walk on */
     if (tile == 1 || tile == 2 || tile == 5 || tile == 6 || tile == 23 || tile == 24 || tile ==7 || tile == 8 || tile ==9 ||
