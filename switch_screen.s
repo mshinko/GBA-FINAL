1bc74a6884bd583eeae5bfc2edc93564f4e0421a
Conor Gill <cgill3@cpsc.umw.edu>
2023-11-30 21:02:48 -0500

    Got everything back after the merge nonsense

---
+++ switch_screen.s
@@ -0,0 +1,14 @@
+/* Filename: switch_screen.s */
+
+/* Define constants for register addresses */
+.equ BG2_CONTROL, 0x400000C
+
+/* Entry point for the switch_screen function */
+.global switch_screen
+switch_screen:
+    ldr r1, =BG2_CONTROL
+    ldr r2, [r1]
+    bic r2, r2, #0x01F00
+    orr r2, r2, r0, lsl #8
+    str r2, [r1]
+    bx lr
