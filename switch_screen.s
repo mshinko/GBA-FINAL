46119a0af42566dd5b5bfbec6aebaaa18d7845a2
Matthew <mshinko@mail.umw.edu>
2023-11-30 20:31:51 -0500

    EVERYTHING DONE

--- switch_screen.s
+++ switch_screen.s
@@ -6,11 +6,9 @@
 /* Entry point for the switch_screen function */
 .global switch_screen
 switch_screen:
-    ldr r1, [r13, #4]      /* Load the parameter from the stack */
-    ldr r0, =BG2_CONTROL
-    ldr r2, [r0]
-    bic r2, r2, #0x3FF     /* Clear the screen block index bits */
-    orr r2, r2, r1, lsl #8 /* Use the parameter as screen_block_index */
-    str r2, [r0]
+    ldr r1, =BG2_CONTROL
+    ldr r2, [r1]
+    bic r2, r2, #0x01F00
+    orr r2, r2, r0, lsl #8
+    str r2, [r1]
     bx lr
-
